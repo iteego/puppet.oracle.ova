e03159de711bf5647ed2803faca09bf58f58ee82
Marcus Pemer <marcus@iteego.com>
2013-01-03 19:05:12 +0100

    Added cwd

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,8 @@
 node localhost {
 
   exec { 'sudoers':
-    command   => "patch /etc/sudoers </etc/puppet/files/etc/sudoers.patch",
+    command   => 'patch sudoers </etc/puppet/files/etc/sudoers.patch',
+    cwd       => '/etc',
     logoutput => true,
     path      => ['/bin', '/usr/bin', '/usr/sbin'],
     unless    => "grep wheel /etc/sudoers | grep -q -v '#'"
