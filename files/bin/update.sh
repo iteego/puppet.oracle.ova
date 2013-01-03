9e8478d5bea835e04118fba9e7119c363ff367dd
Marcus Pemer <marcus@iteego.com>
2013-01-03 18:10:53 +0100

    Added initial update script

---
+++ files/bin/update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+if [ $(id -u) -ne 0 ];
+then
+  echo "You must be root to run this script. Exiting."
+  exit 1
+fi
+
+cd /etc/puppet
+git reset --hard
+git clean -df
+git pull origin master
+puppet apply --verbose --debug /etc/puppet/manifests/init.pp
