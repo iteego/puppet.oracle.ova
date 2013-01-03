a97ddece3f71c7b3a82815462a5f0957bec31eb9
Marcus Pemer <marcus@iteego.com>
2013-01-03 18:56:59 +0100

    Added sudoers magic

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,2 +1,9 @@
 node localhost {
+
+  exec { 'sudoers':
+    command => "patch /etc/sudoers </etc/puppet/files/etc/sudoers.patch"
+    path    => ["/usr/bin", "/usr/sbin"],
+    unless  => "grep wheel /etc/sudoers | grep -q -v '#'"
+  }
+
 }
