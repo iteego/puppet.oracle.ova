be70eeab63e85d016ac3a80e9dd7f6ce9ffc3bc9
Marcus Pemer <marcus@iteego.com>
2013-01-03 19:01:32 +0100

    Corrected path

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,7 @@ node localhost {
   exec { 'sudoers':
     command   => "patch /etc/sudoers </etc/puppet/files/etc/sudoers.patch",
     logoutput => true,
-    path      => ["/usr/bin", "/usr/sbin"],
+    path      => ['/bin', '/usr/bin', '/usr/sbin'],
     unless    => "grep wheel /etc/sudoers | grep -q -v '#'"
   }
 
