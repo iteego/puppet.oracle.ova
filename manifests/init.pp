360f63ae58997f1d6147142b27ac8fee73bcaf45
Marcus Pemer <marcus@iteego.com>
2013-01-03 19:00:24 +0100

    Corrected syntax error in puppet manifest

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,10 @@
 node localhost {
 
   exec { 'sudoers':
-    command => "patch /etc/sudoers </etc/puppet/files/etc/sudoers.patch"
-    path    => ["/usr/bin", "/usr/sbin"],
-    unless  => "grep wheel /etc/sudoers | grep -q -v '#'"
+    command   => "patch /etc/sudoers </etc/puppet/files/etc/sudoers.patch",
+    logoutput => true,
+    path      => ["/usr/bin", "/usr/sbin"],
+    unless    => "grep wheel /etc/sudoers | grep -q -v '#'"
   }
 
 }
